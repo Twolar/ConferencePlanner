06a5df8209ef218d5459a44b17f3e195dcc84610
Taylor Bennett <t4ybennett@gmail.com>
2019-08-28 18:28:13 +1200

    Created DTO, starting to build out BackService

--- BackEnd/Models/Speaker.cs
+++ BackEnd/Models/Speaker.cs
@@ -1,17 +1,10 @@
 using System.ComponentModel.DataAnnotations;
+using ConferenceDTO;
+
 
 namespace BackEnd.Models {
-    public class Speaker {
-        public int ID { get; set; }
-
-        [Required]
-        [StringLength(200)]
-        public string Name { get; set; }
-
-        [StringLength(4000)]
-        public string Bio { get; set; }
-
-        [StringLength(1000)]
-        public virtual string WebSite { get; set; }
+    public class Speaker : ConferenceDTO.Speaker 
+    {
+        
     }
 }
